7071d351d23fc2c4f8a45ae2e86b6dcae33f2193
pointdexter1313-dotcom <pointdexter1313@gmail.com>
2026-06-04 22:58:14 +0700

    Update App.jsx

--- english-champion/src/App.jsx
+++ english-champion/src/App.jsx
@@ -1,4 +1,4 @@
-mport { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 // ============================================================
 // THAI COLOR PALETTE
